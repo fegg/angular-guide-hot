7ec3d7e80b1b480549fc05c6baa3f601d272d0ac
lulin <lulin@gridsum.com>
2018-05-28 09:10:25 +0800

    完整配置

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+console.log(environment);
+console.log('is hot: ', module.hot);
+
+// https://github.com/angular/angular-cli/issues/10663
+if (environment.hmr) {
+  if (module.hot) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap();
+}
